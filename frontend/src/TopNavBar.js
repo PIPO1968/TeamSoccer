9b252244d2f1cf6f5a1dd378b187e8098d05b8d6
PIPO1968 <pipo1968@gmail.com>
2025-10-17 19:17:01 +0100

    fix: eliminar Club, Partidos y Amistosos del sidebar horizontal y mantenerlo fijo para usuarios logueados

--- frontend/src/TopNavBar.js
+++ frontend/src/TopNavBar.js
@@ -5,9 +5,6 @@ function TopNavBar() {
         <nav style={{ position: 'fixed', top: 64, left: 0, width: '100%', background: '#1a2a44', height: 48, zIndex: 999, display: 'flex', alignItems: 'center', boxShadow: '0 1px 6px #0002' }}>
             <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: '0 24px', gap: 32 }}>
                 <li><a href="/home" style={navStyle}>Inicio</a></li>
-                <li><a href="/teams" style={navStyle}>Club</a></li>
-                <li><a href="/matches" style={navStyle}>Partidos</a></li>
-                <li><a href="/friendlies" style={navStyle}>Amistosos</a></li>
                 <li><a href="/store" style={navStyle}>Tienda</a></li>
                 <li><a href="/rooms" style={navStyle}>Salas</a></li>
                 <li><a href="/community" style={navStyle}>Comunidad</a></li>
